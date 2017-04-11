0fbde9d9d5d57fe4ed61369596aec8b5d72e1ec8
nippolito <nico31737@gmail.com>
2017-04-11 16:17:44 -0300

    anda el de dinamica

--- tests.cpp
+++ tests.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <chrono>
 #include <random>
-#include "BacktrackPoda.cpp"
+#include "ProgDinam.cpp"
 
 // g++ -std=c++11 -o name file.cpp
 
@@ -22,7 +22,7 @@ void Ej1(){
 	vec5[9] = 0;
 	vec5[10] = 9;
 	vec5[11] = 3;
-	int c = Colorear(vec5, vec5.size());
+	int c = MinSinPintarProgDinam(vec5, vec5.size());
 	assert(c == 2);
 	cout << "Ejemplo 2 de la practica pasado" << endl;
 }
@@ -37,7 +37,7 @@ void Ej2(){
 	vec3[5] = 1;
 	vec3[6] = 5;
 	vec3[7] = 0;
-	int c = Colorear(vec3, vec3.size());	
+	int c = MinSinPintarProgDinam(vec3, vec3.size());	
 	assert(c == 0);
 	cout << "Ejemplo 1 de la práctica pasado" << endl;
 }
@@ -47,7 +47,7 @@ void MismoNum3Veces(){
 	vec6[0] = 3;
 	vec6[1] = 3;
 	vec6[2] = 3;
-	int c = Colorear(vec6, vec6.size());
+	int c = MinSinPintarProgDinam(vec6, vec6.size());
 	assert(c == 1);
 	cout << "Arreglo de long 3 con los 3 números igual funciona bien" << endl;
 }
@@ -57,7 +57,7 @@ void MismoNumNVeces(int n){
 	for(int i = 0; i < n; i++){
 		vec[i] = 5;
 	}
-	int c = Colorear(vec, vec.size());
+	int c = MinSinPintarProgDinam(vec, vec.size());
 	assert(c == n-2);
 	cout << "Arreglo de long " << n << " con los " << n << " números iguales funciona bien" << endl;
 }
@@ -67,7 +67,28 @@ void Prueba(){			//
 	vec[0] = 3;
 	vec[1] = 2;
 	vec[2] = 7;
-	Colorear(vec, vec.size());
+	MinSinPintarProgDinam(vec, vec.size());
+}
+
+void Prueba1(){			// 
+	std::vector<int> vec(4);
+	vec[0] = 3;
+	vec[1] = 2;
+	vec[2] = 7;
+	vec[3] = 9;
+	MinSinPintarProgDinam(vec, vec.size());
+}
+
+void Prueba2(){			// 
+	std::vector<int> vec(6);
+	vec[0] = 3;
+	vec[1] = 2;
+	vec[2] = 7;
+	vec[3] = 6;
+	vec[4] = 8;
+	vec[5] = 11;
+	// vec[6] = 4;
+	MinSinPintarProgDinam(vec, vec.size());
 }
 
 void MaxMinMaxMin(){				// agregar a Tests algunos de estos
@@ -107,14 +128,10 @@ void MaxMinMaxMin(){				// agregar a Tests algunos de estos
 
 }
 
-void instanOrd(){
-
-}
-
-void writeFile(){										// este test genera para todo n entre 1 y 25, un total de 80 casos aleatorios de longitud n + 10 ordenados crec + 10 ordenados decrec
+void writeFile(){										// este test genera para todo n entre 1 y 100, un total de 80 casos aleatorios de longitud n + 10 ordenados crec + 10 ordenados decrec
 	fstream e("ent.txt", ios::out);
-	for(int n = 1; n < 26; n++){						// genera 80 casis para n entre 1 y 25 random
-		for(int i = 0; i < 80; i++){					// genera los 40 casos de cada n al azar
+	for(int n = 1; n < 101; n++){						// genera 80 casos para n entre 1 y 25 random
+		for(int i = 0; i < 80; i++){					// genera los 80 casos de cada n al azar
 			e << n << endl;
 			for(int j = 0; j < n; j++){
 				int v = rand() % 100000;				// v es un entero que está en el rango [0, 100000]
@@ -127,7 +144,7 @@ void writeFile(){										// este test genera para todo n entre 1 y 25, un tota
 		}
 	}
 
-	for(int y = 1; y < 26; y++){					// genera 10 casos para n entre 1 y 25, ordenados creciente
+	for(int y = 1; y < 101; y++){					// genera 10 casos para n entre 1 y 25, ordenados creciente
 		int prim = 0;
 		for(int h = 0; h < 10; h++){
 			e << y << endl;
@@ -142,7 +159,7 @@ void writeFile(){										// este test genera para todo n entre 1 y 25, un tota
 		}
 	}
 
-	for(int y = 1; y < 26; y++){					// genera 10 casos para n entre 1 y 25, ordenados decreciente
+	for(int y = 1; y < 101; y++){					// genera 10 casos para n entre 1 y 25, ordenados decreciente
 		int prim = 9;
 		for(int h = 0; h < 10; h++){
 			e << y << endl;
@@ -160,17 +177,31 @@ void writeFile(){										// este test genera para todo n entre 1 y 25, un tota
 	e.close();										
 }
 
-void experimento(){
-
+void writeFileParaCorrec(){
+	fstream e("entCorrec.txt", ios::out);
+	for(int n = 1; n < 31; n++){						// genera 10 casos para n entre y 31 random
+		for(int i = 0; i < 10; i++){					// genera los 40 casos de cada n al azar
+			e << n << endl;
+			for(int j = 0; j < n; j++){
+				int v = rand() % 100000;				// v es un entero que está en el rango [0, 100000]
+				if(j < (n-1)){
+					e << v << " ";
+				}else{
+					e << v << endl;
+				}
+			}	
+		}
+	}
+	e.close();
 }
 
-void expSinYConPodaParaCorrec(){					// debes correr este algoritmo y chequear si son iguales salidasSinPoda y salidasPoda
-	writeFile();
+void expParaCorrec(){					// debes correr este algoritmo y chequear si son iguales salidasSinPoda y salidasPoda y salidasDinam ---- OK
+	writeFileParaCorrec();
 
-	fstream e ("ent.txt", ios::in | ios::out);
+	fstream e ("entCorrec.txt", ios::in | ios::out);
 	fstream s ("salidasSinPoda.txt", ios::out);
 
-	for(int i = 0; i < (40 * 40); i++){
+	for(int i = 0; i < (10 * 30); i++){
 		int n;
 		e >> n;
 		std::vector<int> ent(n);
@@ -186,10 +217,10 @@ void expSinYConPodaParaCorrec(){					// debes correr este algoritmo y chequear s
 	e.close();
 	s.close();
 
-	fstream e2 ("ent.txt", ios::in | ios::out);
+	fstream e2 ("entCorrec.txt", ios::in | ios::out);
 	fstream s2 ("salidasPoda.txt", ios::out);
 
-	for(int i = 0; i < (40 * 40); i++){
+	for(int i = 0; i < (10 * 30); i++){
 		int n;
 		e2 >> n;
 		std::vector<int> ent2(n);
@@ -204,6 +235,27 @@ void expSinYConPodaParaCorrec(){					// debes correr este algoritmo y chequear s
 
 	e2.close();
 	s2.close();
+
+	fstream e3 ("entCorrec.txt", ios::in | ios::out);
+	fstream s3 ("salidasDinamica.txt", ios::out);
+
+	for(int i = 0; i < (10 * 30); i++){
+		int n;
+		e3 >> n;
+		std::vector<int> ent3(n);
+		int aux;
+		for(int j = 0; j < n; j++){
+			e3 >> aux;
+			ent3[j] = aux;
+		}
+		int res = MinSinPintarProgDinam(ent3, n);
+		s3 << res << endl;
+	}
+
+	e3.close();
+	s3.close();
+
+
 }
 
 void expSinPodaParaTiempo(){
@@ -293,6 +345,50 @@ void expConPodaParaTiempo(){			// estoy corriendo primero el SinPoda para que me
 	s.close();
 }
 
+void expDinamicaParaTiempo(){
+	writeFile();
+
+	fstream e ("ent.txt", ios::in | ios::out);
+	fstream s ("salidasDinamicaTiempo.csv", ios::out);
+
+	s << "Long_Entrada,Tiempo_ms,Tipo" << endl;
+
+	std::chrono::time_point<std::chrono::system_clock> start, end;
+
+	for(int i = 0; i < (100 * 80 + 100 * 10 + 100 * 10); i++){
+		int n;
+		e >> n;
+		s << n;
+		s << ",";
+		std::vector<int> ent(n);
+		int aux;
+		for(int j = 0; j < n; j++){
+			e >> aux;
+			ent[j] = aux;
+		}
+		start = std::chrono::system_clock::now();
+		MinSinPintarProgDinam(ent, n);
+		end = std::chrono::system_clock::now();
+		std::chrono::duration<double, std::milli> elapsed_seconds = end-start;
+		s << elapsed_seconds.count();
+		s << ",";
+		if(i < 100*80){
+			s << "Random" << endl;
+		}else{
+			if(i >= 100*80 && i < (100*10 + 100*10)){
+				s << "Creciente" << endl;
+			}else{
+				s << "Decreciente" << endl;
+			}
+		}
+	}
+
+	// std::cout << "Sin poda tardó: " << elapsed_seconds.count() << " ms" << endl;
+
+	e.close();
+	s.close();
+}
+
 void correrTestsPracticaBacktrackConPoda(){			// salvo los de entrada 200, son todos correctos.
 	std::chrono::time_point<std::chrono::system_clock> start, end;
 
@@ -347,6 +443,32 @@ void correrTestsPracticaBacktrackSinPoda(){			// salvo los de entrada 200, son t
 	}
 }
 
+void correrTestsPracticaDinamica(){				// son todos correctos
+	std::chrono::time_point<std::chrono::system_clock> start, end;
+
+	fstream s ("salidasTestsCatedraDinamica.txt", ios::out);
+	for(int i = 1; i < 21; i++){
+		string st = "t" + std::to_string(i);
+		fstream e (st, ios::in);
+		int n;
+		e >> n;
+		std::vector<int> ent(n);
+		int aux;
+		for(int j = 0; j < n; j++){
+			e >> aux;
+			ent[j] = aux;
+		}
+		start = std::chrono::system_clock::now();
+		int res = MinSinPintarProgDinam(ent, n);
+		end = std::chrono::system_clock::now();
+		std::chrono::duration<double, std::milli> elapsed_seconds = end-start;			
+		s << res;
+		s << ",";
+		s << elapsed_seconds.count() << endl;
+	}	
+}
+
+
 int main(){				// agregarle varios tests (ver cómo carajo se testeaba jaja, dos tests pueden ser los de la cátedra, otro todos los números iguales, etc...)
 	// Ej1();
 	// Ej2();
@@ -356,8 +478,13 @@ int main(){				// agregarle varios tests (ver cómo carajo se testeaba jaja, dos
 	// MismoNumNVeces(6);
 	// MismoNumNVeces(7);
 	// MismoNumNVeces(12);
+	// expParaCorrec();
+	// correrTestsPracticaDinamica();
+	expDinamicaParaTiempo();
 	// Prueba();
-	expSinPodaParaTiempo();
+	// Prueba1();
+	// Prueba2();
+	// expSinPodaParaTiempo();
 	// expConPodaParaTiempo();
 	// MaxMinMaxMin();
 	// correrTestsPracticaBacktrackSinPoda();
